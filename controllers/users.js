df4750f51659b6c0e5ce241e40a24522080fc364
yisu-kim <yisu.kim.dev@outlook.com>
2020-05-31 14:08:45 +0900

    [feat] Join Controller 구현
    * join 페이지에 form 추가
    * social login 버튼 추가
    * getJoin, postJoin 라우팅 분리

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,23 @@
 /* eslint-disable import/prefer-default-export */
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // To Do: Register user
+    // To Do: Log user in
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
--- routes/global.js
+++ routes/global.js
@@ -1,13 +1,16 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videos";
-import { join, login, logout } from "../controllers/users";
+import { getJoin, postJoin, login, logout } from "../controllers/users";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
