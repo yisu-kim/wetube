16e6a8559687505896e26eefcf24247f6268c496
yisu-kim <yisu.kim.dev@outlook.com>
2020-05-31 19:52:48 +0900

    [feat] Upload Controller 구현
    * upload 페이지에 form 추가
    * form에 비디오 파일 포함
    * getUpload, postUpload 라우팅 분리

--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,8 @@
 import express from "express";
 import routes from "../routes";
 import {
-  upload,
+  getUpload,
+  postUpload,
   videoDetail,
   editVideo,
   deleteVideo,
@@ -9,8 +10,10 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
