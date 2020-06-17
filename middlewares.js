b892f97c5a063db7800e41a9213565e1e6ae494b
yisukim <yisu.kim.dev@outlook.com>
2020-06-17 22:24:23 +0900

    [feat] Footer SCSS 작성
    * footer.scss 구현

--- middlewares.js
+++ middlewares.js
@@ -6,7 +6,7 @@ export const locals = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1,
   };
   next();
